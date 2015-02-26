1b667b239dc2d32970d1baab1ca5287e60af47f6
William Tsang <williamkt91@gmail.com>
2015-02-25 22:57:08 -0800

    Added nav and footer

--- public/application.js
+++ public/application.js
@@ -5,42 +5,19 @@ $(document).ready(function(){
 
   $("#nav a").click(function(e){
     e.preventDefault();
-    console.log("this is e");
-    console.log(e);
-    $('html,body').scrollTo(this.hash,this.hash)
-    console.log("this is this");
-    console.log(this);
-    console.log("this is hash");
-    console.log(this.hash);
+    $('html,body').scrollTo(this.hash,this.hash);
   });
 
   // END NAV SMOOTH SCROLL
 
   // SCREEN RESIZING
 
-  // window.addEventListener('resize', draw, false);
-
-  // function draw(){
-  //   var width = $(document).width();
-  //   var height = $(document).height();
-  //   console.log("Width: " + width);
-  //   console.log("Height: " + height);
-  //   $("body").width(width).height(height);
-  // }
-  // $(document).resize(draw);
-
+  // $(window).resize(function(){
+  //   $(".section").text("hello")
+  // });
 
   // END SCREEN RESIZING
 
-// function resize(){
-//     // do all your stuff here
-
-// }
-
-// $(document).ready(function(){
-//     resize();
-//     $(window).resize(resize);
-// });
 
 
 }); // end doc
