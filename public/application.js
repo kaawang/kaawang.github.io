f5f993d0327039ec95bc600868db57a681b9ab50
William Tsang <williamkt91@gmail.com>
2015-02-27 17:02:34 -0800

    Tada first page might be done

--- public/application.js
+++ public/application.js
@@ -1,23 +1,19 @@
 $(document).ready(function(){
 
-
   // NAV SMOOTH SCROLL
-
   $("#nav a").click(function(e){
     e.preventDefault();
+
+    // NAV HIGHLIGHT
+    var linkList = $(".link");
+    for (var i = linkList.length - 1; i >= 0; i--) {
+      linkList[i].style.backgroundColor = "#000000";
+    };
+    this.style.backgroundColor = "#87CEEB";
+    // END NAV HIGHLIGHT
+
     $('html,body').scrollTo(this.hash,this.hash);
   });
-
   // END NAV SMOOTH SCROLL
 
-  // SCREEN RESIZING
-
-  // $(window).resize(function(){
-  //   $(".section").text("hello")
-  // });
-
-  // END SCREEN RESIZING
-
-
-
-}); // end doc
+}); // END DOC
