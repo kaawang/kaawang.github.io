6efaaba8216e0189f95e2fd4c3dadb90a63dd43a
William Tsang <williamkt91@gmail.com>
2015-02-28 11:25:52 -0800

    Draft about me complete

--- public/application.js
+++ public/application.js
@@ -16,4 +16,26 @@ $(document).ready(function(){
   });
   // END NAV SMOOTH SCROLL
 
+// KEY TO SECTION // need to use bind function to retain this value of DOM
+  // document.addEventListener("keydown", scrollToNextSection, false)
+
+  // function scrollToNextSection(event) {
+  //     // $('html,body').scrollTo(this.hash, this.hash);
+  //   if (event.keyCode === 37) {
+  //     console.log("left");
+  //   } else if (event.keyCode === 38) {
+  //     console.log("up");
+  //     console.log(this)
+  //   } else if (event.keyCode === 39) {
+  //     console.log("right");
+  //   } else if (event.keyCode === 40) {
+  //     console.log("down");
+  //     console.log(this)
+  //   } else {
+  //     console.log("not a directional key");
+  //   }
+  // }
+
+// END KEY TO SECTION
+
 }); // END DOC
